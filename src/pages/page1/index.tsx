878244761eb5620e093febc1d885159cd0d96d67
eminguyen <emilyhuongnguyen@gmail.com>
2020-03-06 22:46:23 -0800

    Add boilerplate

---
+++ src/pages/page1/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./style.less";
+
+const Page1: React.FC = () => {
+  return (
+    <div className="page1">
+      <h1></h1>
+    </div>
+  );
+};
+
+export default Page1;
