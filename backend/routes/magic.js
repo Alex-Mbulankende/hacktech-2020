901a515744a282fc3d42674093f800d740d1381e
Ronak Shah <ronakshahultimate@gmail.com>
2020-03-07 19:56:02 -0800

    keshav sucks

--- backend/routes/magic.js
+++ backend/routes/magic.js
@@ -1,6 +1,11 @@
 const express = require('express');
-const router = express.Router();
+const firebase = require('firebase')
 const utils = require("../../seed/utils")
+
+const app = firebase.initializeApp(utils.firebaseConfig)
+const router = express.Router();
+
+
 /* GET home page. */
 router.get('/listings', async function(req, res, next) {
     let zip = req.query.zip;
@@ -9,7 +14,9 @@ router.get('/listings', async function(req, res, next) {
     let radiusUnit = req.query.radiusUnit;
     let limit = req.query.limit;
 
-    let eBay = require('ebay-node-client')(utils.prod_key, utils.prod_secret);
+    let eBay = require('ebay-node-client')(utils.sbx_key, utils.sbx_secret);
+    // let eBay = require('ebay-node-client')(utils.prod_key, utils.prod_secret);
+    
     await getOAuthToken(eBay);
     await setUserToken(eBay);
     let data = await searchByZipCode(eBay, country, zip, raidus, radiusUnit, limit);
@@ -32,21 +39,63 @@ async function getOAuthToken(eBay) {
 }
 
 async function setUserToken(eBay) {
-    eBay.setUserToken(utils.prod_USER_token);
+    eBay.setUserToken(utils.sbx_USER_token);
 }
 
+let categoryNames = {
+    masks: "257818",
+    handSanitizer: "177663",
+    camping: "181381",
+    medicine: "75036"
+}
+
+let nameForNumber = {
+    "257818": "masks",
+    "177663": "handSanitizer",
+    "181381": "camping",
+    "75036": "medicine"
+}
 
 async function searchByZipCode(eBay, country="US", zipcode=92129, radius=30, unit="mi", limit=3){
-    categories = ["257818", "177663", "181381", "75036"];
+    // debugger
+    categories = [categoryNames.masks, categoryNames.handSanitizer, categoryNames.camping, categoryNames.medicine];
     var data = {
         category_ids: "220",
         filter: (`pickupCountry:${country},pickupPostalCode:${zipcode},pickupRadius:${radius},pickupRadiusUnit:${unit},deliveryOptions:{SELLER_ARRANGED_LOCAL_PICKUP}`),
         limit: `${limit}`
     };
     try {
-        var response = await eBay.browse.search(data);
-        console.log('response', response);
-        return response
+        let status = {}
+        let catFunc = function(catId) {
+            return async function() {
+                let data = {
+                    category_ids: `${catId}`,
+                    filter: (`pickupCountry:${country},pickupPostalCode:${zipcode},pickupRadius:${radius},pickupRadiusUnit:${unit},deliveryOptions:{SELLER_ARRANGED_LOCAL_PICKUP}`),
+                    limit: `${limit}`,
+                    fieldgroups: "FULL"
+                };
+                let response = await eBay.browse.search(data);
+                status[catId] = response.total
+                let result = [];
+                // await Promise.all(response.itemSummaries.map(item => {
+                //     return async function() {
+                //         let id = item.itemId;
+                //         let itemData = await eBay.browse.getItem(id);
+                //         result.push(itemData)
+                //     }
+                // }))
+                
+                return response;
+            }
+        }
+        let results = await Promise.all(categories.map(catId => catFunc(catId)()));
+
+        // var response = await eBay.browse.search(data);
+        categories.forEach(cat => {
+            firebase.database().ref(`zipcodes/${zipcode}`).child(nameForNumber[cat]).set(status[cat])
+            console.log(`${nameForNumber[cat]} -> ${status[cat]}`)
+        })
+        return results
     } catch (error) {
         console.log('error ', error);
         return;
