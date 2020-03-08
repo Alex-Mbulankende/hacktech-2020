771051d57ddd49d3b2e881d38ede007790208eea
Ronak Shah <ronakshahultimate@gmail.com>
2020-03-07 22:54:02 -0800

    time to mock data

--- backend/routes/magic.js
+++ backend/routes/magic.js
@@ -59,11 +59,7 @@ let nameForNumber = {
 async function searchByZipCode(eBay, country="US", zipcode=92129, radius=30, unit="mi", limit=3){
     // debugger
     categories = [categoryNames.masks, categoryNames.handSanitizer, categoryNames.camping, categoryNames.medicine];
-    var data = {
-        category_ids: "220",
-        filter: (`pickupCountry:${country},pickupPostalCode:${zipcode},pickupRadius:${radius},pickupRadiusUnit:${unit},deliveryOptions:{SELLER_ARRANGED_LOCAL_PICKUP}`),
-        limit: `${limit}`
-    };
+    
     try {
         let status = {}
         let catFunc = function(catId) {
@@ -76,20 +72,23 @@ async function searchByZipCode(eBay, country="US", zipcode=92129, radius=30, uni
                 };
                 let response = await eBay.browse.search(data);
                 status[catId] = response.total
-                let result = [];
-                debugger
-                // console.log(response)
-                // if (response.total > 0) {
-                //     await Promise.all(response.itemSummaries.map(item => {
-                //         return async function() {
-                //             let title = item.title
-                //             let data = firebase.database().ref(`/listings/${title}`)
-                //         }
-                //     }))  
-                // }
-                
-                
-                return response;
+                let allData = [];
+
+                if (response.total > 0) {
+                    for await (const item of response.itemSummaries) {
+                        let id = item.itemId;
+                        let data = await firebase.database().ref(`/listings/${id}`).once('value').then(async function(snapshot) {
+                            const result = await snapshot.val()
+                            return result || id
+                        })
+                        console.log(data)
+                        allData.push(data)
+                    }
+                    console.log(allData)
+                    return allData;
+                } else {
+                    return response
+                }
             }
         }
         let results = await Promise.all(categories.map(catId => catFunc(catId)()));
@@ -97,8 +96,9 @@ async function searchByZipCode(eBay, country="US", zipcode=92129, radius=30, uni
         // var response = await eBay.browse.search(data);
         categories.forEach(cat => {
             firebase.database().ref(`zipcodes/${zipcode}`).child(nameForNumber[cat]).set(status[cat]/10)
-            console.log(`${nameForNumber[cat]} -> ${status[cat]}`)
+            // console.log(`${nameForNumber[cat]} -> ${status[cat]}`)
         })
+        // console.log(results)
         return results
     } catch (error) {
         console.log('error ', error);
@@ -188,7 +188,7 @@ function addItem(title,
             if (!error && response.statusCode == 200) {
                 // add to firebase too
                 let itemId = body.body.split("=").pop()
-                firebase.database().ref(`${itemId}`).set({
+                firebase.database().ref(`v1|${itemId}|0`).set({
                     title: title,
                     description: description,
                     categoryID: categoryID,
