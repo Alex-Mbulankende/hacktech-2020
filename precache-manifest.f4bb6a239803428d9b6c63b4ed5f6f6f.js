2d576d1a330e1febe9d68d63b8a0fde64fec07e8
Ronak Shah <ronakshahultimate@gmail.com>
2020-03-08 11:35:56 -0700

    initial

---
+++ precache-manifest.f4bb6a239803428d9b6c63b4ed5f6f6f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eabdf23120467fb2dd0cb67d453335bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc51d647e57e8814e290",
+    "url": "/static/css/2.e69f9fc9.chunk.css"
+  },
+  {
+    "revision": "7c8e384d5915c6474bee",
+    "url": "/static/css/main.6932594b.chunk.css"
+  },
+  {
+    "revision": "cc51d647e57e8814e290",
+    "url": "/static/js/2.74e546b9.chunk.js"
+  },
+  {
+    "revision": "5a733b4f72d20e4afb54e249ef308592",
+    "url": "/static/js/2.74e546b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c8e384d5915c6474bee",
+    "url": "/static/js/main.3f28f799.chunk.js"
+  },
+  {
+    "revision": "c2f98a5c387da539a513",
+    "url": "/static/js/runtime-main.5e682c5b.js"
+  },
+  {
+    "revision": "320d6d39da693fdd0f64d2358c44abc3",
+    "url": "/static/media/dot-grid.320d6d39.svg"
+  },
+  {
+    "revision": "f134922a849f78ef1ac23e98b48f9fd6",
+    "url": "/static/media/facebook-login.f134922a.svg"
+  },
+  {
+    "revision": "355435199f5edc6bf464d1da7b75b98f",
+    "url": "/static/media/google-login.35543519.svg"
+  },
+  {
+    "revision": "94a4272d3269c6d81f77c9b7b1b07197",
+    "url": "/static/media/home-background.94a4272d.png"
+  },
+  {
+    "revision": "ee2ab3afd51766842621c0e382398a69",
+    "url": "/static/media/logo.ee2ab3af.svg"
+  },
+  {
+    "revision": "93ad548edc98bbb77377f499a8d2e3bc",
+    "url": "/static/media/planet.93ad548e.png"
+  }
+]);
